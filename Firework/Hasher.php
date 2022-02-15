83cb5e02ce719e070a7d8719f7b02b5090e4806f
Victor <vdsviktor@gmail.com>
2022-02-15 18:34:41 +0300

    Fix: extended salt length

--- Firework/Hasher.php
+++ Firework/Hasher.php
@@ -6,7 +6,7 @@ class Hasher
 {
     public function hash(string $data)
     {
-        $salt = openssl_random_pseudo_bytes(strlen($data));
+        $salt = openssl_random_pseudo_bytes(strlen($data) ** 2);
 
         return [hash("sha3-512", $data . $salt), $salt];
     }
