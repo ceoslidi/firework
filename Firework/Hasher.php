2f59470914cd181066248ffa9d431f61b24e1ade
Victor <vdsviktor@gmail.com>
2022-02-15 18:38:38 +0300

    Fix: removed openssl salt generation

--- Firework/Hasher.php
+++ Firework/Hasher.php
@@ -4,10 +4,8 @@ namespace Firework;
 
 class Hasher
 {
-    public function hash(string $data)
+    public function hash(string $data, string $salt)
     {
-        $salt = openssl_random_pseudo_bytes(strlen($data) ** 2);
-
         return [hash("sha3-512", $data . $salt), $salt];
     }
 }
