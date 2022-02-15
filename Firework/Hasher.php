3f64dcaa1df03be3b9393f14e239186af2a6e41e
Victor <vdsviktor@gmail.com>
2022-02-15 18:31:04 +0300

    Fix: hash generation class

--- Firework/Hasher.php
+++ Firework/Hasher.php
@@ -4,10 +4,9 @@ namespace Firework;
 
 class Hasher
 {
-    public function hash(string $data, string $salt)
+    public function hash(string $data)
     {
-        if (!$salt)
-            $salt = openssl_random_pseudo_bytes(strlen($data));
+        $salt = openssl_random_pseudo_bytes(strlen($data));
 
         return [hash("sha3-512", $data . $salt), $salt];
     }
