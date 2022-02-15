e00b4ff6053aa34174c1d3ef9bbf464b6e63e538
Yan Svintsov <ceo@slidi.ru>
2022-02-15 22:16:36 +0400

    Fix: add PHPDoc to Hash::hash

--- firework/Hash.php
+++ firework/Hash.php
@@ -6,6 +6,10 @@ use Config\Config;
 
 class Hash
 {
+    /**
+     * @param string $data
+     * @return array
+     */
     public function hash(string $data): array
     {
         $config = new Config();
