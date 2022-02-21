594fb9b53869b2046a8e4a55786a74ec8d21173e
ceoslidi <ceo@slidi.ru>
2022-02-21 22:06:01 +0300

    Fix: Release

--- index.php
+++ index.php
@@ -4,9 +4,4 @@ require './app/bootstrap.php';
 
 use Firework\Router;
 
-use App\Controllers\HomeController;
-
 $router = new Router();
-
-$router->get('/home', [HomeController::class, 'render']);
-$router->post('/home', [HomeController::class, 'filer']);
